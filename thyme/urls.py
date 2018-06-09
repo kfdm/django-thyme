b4a42cf2c0decddd628d0920675f86de578c3bbe
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-06-09 14:51:30 +0900

    Django2 compatibility

--- thyme/urls.py
+++ thyme/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from thyme import views
 
+app_name = 'thyme'
 urlpatterns = [
     url(r'snapshot$', views.SnapshotList.as_view(), name='snapshot-list'),
     url(r'blacklist$', views.BlacklistList.as_view(), name='blacklist-list'),
